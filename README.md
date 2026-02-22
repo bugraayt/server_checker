af2cf94e936fc8f41d9542933b0fe5387cea5756
Bugra Ayten <bugraayten@Bugras-MacBook-Air.local>
2026-02-22 03:30:21 +0100

    Initial commit: Server Checker

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Server Status Checker
 A project that shows if your server is up or down
+
+Bugra Ayten
--- server_checker.sh
+++ server_checker.sh
@@ -9,10 +9,10 @@ do
 	do
 		echo "Checking $server"
 		if ping -q -c 1 -w 3 $server > /dev/null; then
-			echo -e "$(date): $server is UP." | tee -a server_status.log
+			echo "$(date): $server is UP." >> "server_status.log"
             echo -e "\e[32m$(date): $server is UP.\e[0m"
 		else
-			echo -e "$(date): $server is currently DOWN." | tee -a server_status.log
+			echo "$(date): $server is currently DOWN." >> "server_status.log"
             echo -e "\e[31m$(date): $server is currently DOWN.\e[0m"
 		fi
 	done
